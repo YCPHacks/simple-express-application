fad3c94689bfc6cf0a41a0528cc9f3fbcb407740
Ralph Greaves <rgreaves@ycp.edu>
2022-08-30 01:05:53 -0400

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.status(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
