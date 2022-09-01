e0b6be2516a80e3131a8ae85fdf2098711cae4a5
Ralph Greaves <rgreaves@ycp.edu>
2022-09-01 18:56:58 -0400

    Update and rename server.js to simple-express-application.js
    - Reflect ability to route to API-specific endpoints.
    - Reflect ability to respond to HTTP requests with various "Content-Types", including JSON.
    - Rename variables to better reflect their importance.

--- src/simple-express-application.js
+++ src/simple-express-application.js
@@ -5,8 +5,12 @@ const app = express();
 
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.status(200).end();
+app.get('/', (request, response) => {
+  response.status(200).end();
+});
+
+app.get('/api', (request, response) => {
+  response.status(200).json({ key: 'value' });
 });
 
 app.listen(port, () => {
